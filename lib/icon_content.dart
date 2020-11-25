5a22a4fb668b5d485d313b2e601e3284d7e9775b
mykola <mykola.tokariev@gmail.com>
2020-11-25 10:06:06 -0100

    Final changes

---
+++ lib/icon_content.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'constants.dart';
+
+class IconContent extends StatelessWidget {
+  final Icon icon;
+  final String text;
+
+  IconContent({this.icon, this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        icon,
+        SizedBox(height: 15.0),
+        Text(
+          text,
+          style: kLabelTextStyle,
+        ),
+      ],
+    );
+  }
+}
